c04885b5f8c89f4b156e5432c168701ab57bc019
karan pawar <karan@vkvtech.in>
2024-03-06 14:35:09 +0530

    collapse expand feature added

--- frontend/src/components/Tickets/TicketDetails.js
+++ frontend/src/components/Tickets/TicketDetails.js
@@ -6,18 +6,15 @@ import Backbutton from "../Backbutton";
 import ticketDetails_api from "../../api_strings/admin/ticketDetails_api";
 import AdminContext from "../../AdminContext";
 import { Button } from "@nextui-org/react";
-import MailSender from "./MailSender";
 import { AiOutlineProfile, AiFillForward, AiOutlineCloseCircle, AiOutlineDelete, AiTwotoneInfoCircle } from "react-icons/ai";
 import MessageReply from "./MessageReply";
 import { CiChat1 } from "react-icons/ci";
 import AddNote from "./AddNote";
 import ForwardTicket from "./ForwardTicket";
-import FileIcon from "../../resources/icons/FileIcon";
-import SpreadSheetIcon from "../../resources/icons/SpreadSheetIcon";
-import PDFIcon from "../../resources/icons/PDFIcon";
-import PhotoIcon from "../../resources/icons/PhotoIcon";
-import VideoIcon from "../../resources/icons/VideoIcon";
-import DeleteOutlinedIcon from "../../resources/icons/DeleteOutlinedIcon";
+import FileCard from "../FileCards/FileCard";
+import { GoSidebarExpand } from "react-icons/go";
+import { GoSidebarCollapse } from "react-icons/go";
+
 
 function BorderButton(props) {
     return (
@@ -65,6 +62,7 @@ export default function TicketDeails({ onUpdateSuccess }) {
     const [isReplyModalOpen, setReplyModalOpen] = useState(false);
     const [isNoteModalOpen, setNoteModalOpen] = useState(false);
     const [isForwardModalOpen, setForwardModalOpen] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(true);
 
 
     useEffect(() => {
@@ -90,6 +88,10 @@ export default function TicketDeails({ onUpdateSuccess }) {
         setForwardModalOpen(prevState => !prevState);
     }
 
+    const toggleExpandCollapse = () => {
+        setIsExpanded(prevState => !prevState);
+    };
+
 
 
 
@@ -104,12 +106,13 @@ export default function TicketDeails({ onUpdateSuccess }) {
                     <BorderButton className="ml-3" onClick={handleForward} startContent={<AiFillForward />}>Forward</BorderButton>
                     <BorderButton className="ml-3" startContent={<AiOutlineCloseCircle />}>Close</BorderButton>
                     <BorderButton className="ml-3" startContent={<AiOutlineDelete />}>Delete</BorderButton>
+                    <BorderButton isIconOnly onClick={toggleExpandCollapse}>{isExpanded ? <GoSidebarCollapse className="text-xl" /> : <GoSidebarExpand className="text-xl" />}</BorderButton>
                 </header>
 
-                {/* Main Content */}
                 <div className="mt-2 flex relative">
-                    {/* Left Content */}
-                    <div className="w-[50%] border-1 p-2">
+
+                    {/* left content */}
+                    <div className={`${isExpanded ? 'w-[50%]' : 'w-[75%]'} border-1 p-2`}>
                         <h2 className="text-2xl font-bold ml-10">{ticketDetailsData.subject}</h2>
                         <div className="flex flex-col gap-4 ml-10">
                             <div>
@@ -145,9 +148,9 @@ export default function TicketDeails({ onUpdateSuccess }) {
                         </div>
                     </div>
 
-                    {/* Right Content */}
-                    <div className="border-1 w-[25%] p-2">
-                        <h2 className="text-lg">Open ticket status</h2>
+                    {/* Middle Content */}
+                    <div  className={`border-1 w-[25%] p-2`}>
+                        <h2 className="text-2xl font-bold">{ticketDetailsData.status}</h2>
                         <div className="mt-6 flex flex-col items-center p-4">
                             <h2 className="text-lg">Properties</h2>
                             <UpdatableElement fieldType="Input" onUpdateSuccess={onUpdateSuccess} id={id} fieldName="subject" fieldValue={ticketDetailsData["subject"]} label="Subject" updateFunction={updateTicket_api} />
@@ -160,25 +163,31 @@ export default function TicketDeails({ onUpdateSuccess }) {
                         </div>
                     </div>
 
-                    <div className="border-1 w-[25%] hover:border-2 hover:border-black p-2">
-                        <div className="flex items-center">
-                            <span className="mr-4"><AiTwotoneInfoCircle /></span>
-                            <h2>CONTACT DETAILS</h2>
+                    {isExpanded && (
+                        <div className="border-1 w-[25%] hover:border-2 hover:border-black p-2">
+                            <>
+                                <div className="flex items-center">
+                                    <span className="mr-4"><AiTwotoneInfoCircle /></span>
+                                    <h2>CONTACT DETAILS</h2>
+                                </div>
+                                <div className="flex flex-col h-[200px]">
+                                    <Link className="text-blue-500 p-3">
+                                        karan pawar
+                                    </Link>
+                                    <div className="flex flex-col mb-2">
+                                        <label className="font-thin">Email</label>
+                                        <span className="font-bold">kdpawar752@gmail.com</span>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <label className="font-thin">Work phone</label>
+                                        <span className="font-bold">8378907753</span>
+                                    </div>
+                                </div>
+                            </>
+
                         </div>
-                        <div className="flex flex-col h-[200px]">
-                            <Link className="text-blue-500 p-3">
-                                karan pawar
-                            </Link>
-                            <div className="flex flex-col mb-2">
-                                <label className="font-thin">Email</label>
-                                <span className="font-bold">kdpawar752@gmail.com</span>
-                            </div>
-                            <div className="flex flex-col">
-                                <label className="font-thin">Work phone</label>
-                                <span className="font-bold">8378907753</span>
-                            </div>
-                        </div>
-                    </div>
+                    )
+                    }
 
 
                     <MessageReply isModalOpen={isReplyModalOpen} setModalOpen={setReplyModalOpen} id={id} setTicketDetailsData={setTicketDetailsData} userNames={userNames} />
@@ -189,59 +198,4 @@ export default function TicketDeails({ onUpdateSuccess }) {
 
         </>
     )
-}
-
-
-
-
-
-
-function FileCard({ name, size }) {
-
-
-
-    return <div className="border border-default-200 rounded-md py-1 px-2">
-        <div className="flex gap-2 items-start">
-            <ExtIcon filename={name} />
-            <div className="flex flex-col gap-1">
-                <div className="text-sm">{name}</div>
-                <div className="text-default-500 text-xs">{getSize(size)}</div>
-            </div>
-
-        </div>
-    </div>
-}
-
-
-const units = ['bytes', 'KB', 'MB', 'GB'];
-
-function getSize(x) {
-
-    let l = 0, n = parseInt(x, 10) || 0;
-
-    while (n >= 1024 && ++l) {
-        n = n / 1024;
-    }
-
-    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
-}
-
-
-function ExtIcon({ filename }) {
-
-    let ext = filename.substring(filename.lastIndexOf('.') + 1);
-
-    if (['xls', 'xlsx', 'xlsb', 'XLS', 'XLSX', 'XLSB', 'csv', 'CSV'].includes(ext)) {
-        return <span className="text-green-500"><SpreadSheetIcon size={16} /></span>
-    } else if (['pdf', 'PDF'].includes(ext)) {
-        return <span className="text-red-500"><PDFIcon size={16} /></span>
-    } else if (['jpg', 'jpeg', 'png', 'gif', 'webp', 'cif', 'JPG', 'JPEG', 'PNG', 'GIF', "CIF", 'WEBP'].includes(ext)) {
-        return <PhotoIcon size={16} />
-    } else if (['mp4', 'mpeg4', 'mpeg', 'avi', 'mkv', 'MP4', 'MPEG4', 'MPEG', 'AVI', 'MKV'].includes(ext)) {
-        return <VideoIcon size={16} />
-    } else {
-        return <FileIcon size={16} />
-    }
-
-
 }
